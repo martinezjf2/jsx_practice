3c0076cab36b1b3a8366b0eca9114dd0b3f46b1c
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-15 22:18:43 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+  </div>,
+  document.getElementById("root")
+);
